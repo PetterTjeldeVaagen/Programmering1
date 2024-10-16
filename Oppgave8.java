a8de758ed4c85b5d6dd14ffc1891d23ad81ea47b
xXMemeMachineXx <101399351+xXMemeMachineXx@users.noreply.github.com>
2024-10-16 12:41:14 +0200

    Update Oppgave8.java

--- Oppgave8.java
+++ Oppgave8.java
@@ -1,9 +1,48 @@
-import java.time.*;
-import java.util.*;
+import static javax.swing.JOptionPane.*;
+import javax.swing.JOptionPane;
 
 public class Oppgave8 {
     public static void main(String[] args) {
+        Person per = new Person("Per", "Hansen",1973);
+        ArbTaker arbeiderPer = new ArbTaker(per, 12, 2003, 30000, 26.7);
 
+        while (true) {
+            String[] options = { "Månedlig Skattetrekk", "Brutto årslønn", "Årlig skatetrekk", "Navn", "Alder", "Antall år ansatt i bedrift","Om personen har jobbet i bedriften i mer en et gitt antall år","Endre lønn", "Endre skatteprosent" ,"Avslutt programmet"};
+            String result =(String) JOptionPane.showInputDialog(null, "Hva vil du gjøre", "valg",
+                JOptionPane.PLAIN_MESSAGE,
+                null, options, options[0]);
+
+            if(result==options[0]){
+                showMessageDialog(null,arbeiderPer.MaanedligSkatt());
+            }else if(result==options[1]){
+                showMessageDialog(null,arbeiderPer.BruttoLoenn());
+            }else if(result==options[2]){
+                showMessageDialog(null,arbeiderPer.TotaltSkattetrekk());
+            }else if(result==options[3]){
+                showMessageDialog(null,arbeiderPer.Navn());
+            }else if(result==options[4]){
+                showMessageDialog(null,arbeiderPer.Alder());
+            }else if(result==options[5]){
+                showMessageDialog(null,arbeiderPer.AarIBedrift());
+            }else if(result==options[6]){
+                int input = Integer.parseInt(showInputDialog("Hvor lenge skal personen ha jobbet?"));
+                if(input<=arbeiderPer.AarIBedrift()){
+                    showMessageDialog(null,"" + arbeiderPer.Navn() +" Har jobbet i bedriften i " + input + " År eller mer!");
+                }else{
+                    showMessageDialog(null,"" + arbeiderPer.Navn() +" Har ikke jobbet i bedriften i " + input + " År eller mer!");
+                }
+            }else if(result==options[7]){
+                int input = Integer.parseInt(showInputDialog("Hva er den nye lønnen?"));
+                arbeiderPer.setMaanedsloenn(input);
+                System.out.println("Ny månedslønn er " + arbeiderPer.getMaanedsloenn());
+            }else if(result==options[8]){
+                int input = Integer.parseInt(showInputDialog("Hva er den nye skatteprosenten?"));
+                arbeiderPer.setSkatteprosent(input);
+                System.out.println("Ny skatteprosent er " + arbeiderPer.getSkatteprosent());
+            }else if(result==options[9]){
+                break;
+            }
+        }
     }
 }
  
@@ -12,7 +51,7 @@ final class Person{
     private String etternavn;
     private int foedselsaar; 
 
-    private Person(String fornavn, String etternavn, int foedselsaar){
+    public Person(String fornavn, String etternavn, int foedselsaar){
         this.fornavn=fornavn;
         this.etternavn=etternavn;
         this.foedselsaar=foedselsaar;
@@ -76,7 +115,7 @@ class ArbTaker{
     }
     
     public double MaanedligSkatt(){
-        return (skatteprosent*maanedsloenn)/100; //hvor mange kroner som månedlig må betales i skatt
+        return (skatteprosent*maanedsloenn)/100;
     }
 
     public int BruttoLoenn(){
@@ -98,15 +137,4 @@ class ArbTaker{
     public int AarIBedrift(){
         return kalender.get(java.util.Calendar.YEAR)-this.ansettelsesaar;
     }
-
-    public boolean  ArbeidsTidSjekk(int antallArr){
-        if(antallArr<AarIBedrift()){
-            return true;
-        }else{
-            return false;
-        }
-    }
-
-
-
 }
